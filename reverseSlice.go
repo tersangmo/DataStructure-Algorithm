d1993269891ae1e616a64aa38f4aa8d078c6e458
Maanisha1 <97993213+Maanisha1@users.noreply.github.com>
2022-07-26 16:16:11 -0700

    anoither one

---
+++ reverseSlice.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func reverse1(s []int) []int {
+	for a, b := 0, len(s)-1; a < b; a, b = a+1, b-1 {
+		s[a], s[b] = s[b], s[a]
+	}
+	return s
+}
+
+func reverse2(s []int) []int {
+	var s1 []int
+	for i := len(s) - 1; i >= 0; i-- {
+		s1 = append(s1, s[i])
+	}
+	return s1
+}
+
+func main() {
+	slice1 := []int{5, 3, 1, 8, 9}
+
+	fmt.Println("The reverse slice without using temporary slice is:", reverse1(slice1))
+	slice2 := []int{2, 1, 7, 8, 9}
+	fmt.Println("The reverse slice with using temporary slice is:", reverse2(slice2))
+
+}
